33b377aa46c4d6f1736cbb0ae42c345f45f87040
Wesley Matos <wesleycostamatos@gmail.com>
2024-05-31 21:53:40 -0300

    feat: sugest style navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/Footer";
 export default function App() {
   return (
     <div className="bg-cover bg-scroll bg-center h-screen w-screen flex items-center justify-center" style={{backgroundImage: "url(/bg-1.jpg)"}}>
-      <div className="w-9/12 h-5/6 flex flex-col text-white text-lg leading-relaxed antialiased hover:subpixel-antialiased tracking-wide bg-blue-950 bg-gradient-to-l from-cyan-950 to-blue-950 opacity-50 rounded-sm">
+      <div className="w-9/12 h-5/6 flex flex-col text-white text-lg leading-relaxed antialiased hover:subpixel-antialiased tracking-wide bg-blue-950 bg-gradient-to-l from-cyan-950 to-blue-950 opacity-75 rounded-sm">
 
         <Header />
         <div className="grow overflow-y-auto text-center py-6" >
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -15,18 +15,18 @@ function Header() {
   const { pathname } = useLocation();
 
   return (
-    <div className="sticky top-0 flex gap-12 p-4">
-      <h1>Logo - Name App</h1>
-      <nav>
+    <div className="sticky top-0 flex items-center gap-12 p-4">
+      <h1 className="text-blue-50">Logo - Name App</h1>
+      <nav className="flex gap-2 py-2 px-3 rounded-lg border border-t-slate-950 border-l-slate-950 border-r-slate-600 border-b-slate-600 shadow-[-1px_-1px_rgba(0,0,0,1),1px_1px_rgba(255,255,255,0.1)]">
         {navigation.map((route) => (
           <Link
             key={route.name}
             to={route.pathname}
             className={classNames(
               route.pathname == pathname
-                ? "bg-blue-900 text-white"
-                : "text-blue-300 hover:bg-blue-700 hover:text-white",
-              "rounded-md px-3 py-2 font-medium"
+                ? "shadow-[1px_1px_1px_1px_rgba(0,0,0,0.5),-1px_-1px_2px_2px_rgba(255,255,255,0.1)] text-blue-50"
+                : "text-blue-300 hover:bg-blue-900 hover:text-blue-50",
+              "rounded-lg px-3 py-2 font-medium"
             )}
           >
             {route.name}
